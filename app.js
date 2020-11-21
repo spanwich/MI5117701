f3f90048b8ec6cdf888bfa1066c0fa6f5fe18293
saranachon iammongkol <xalanaseon@hotmail.com>
2020-11-21 17:59:30 +0800

    version 3

--- app.js
+++ app.js
@@ -32,7 +32,7 @@ passport.use(new GoogleStrategy({
   },
   function(request, accessToken, refreshToken, profile, done) {
 	console.log("access token: ", accessToken);
-	console.log(profile);
+	console.log(profile.id);
     //User.findOrCreate({ googleId: profile.id }, function (err, user) {
     //  return done(err, user);
     //});
