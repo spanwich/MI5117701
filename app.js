a889d4becb9c07cd6854efdd9ea0820357bb8ee8
saranachon iammongkol <xalanaseon@hotmail.com>
2020-11-21 15:13:10 +0800

    change databse + google auth version 2

--- app.js
+++ app.js
@@ -17,6 +17,7 @@ const database = require('./routes/database');
 const app = express();
 
 const {login, refresh} = require('./authentication')
+const {google, callback, success, failure} = require('./googleauth')
 const {verify} = require('./middleware')
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
@@ -50,16 +51,9 @@ app.use('/api/database',verify, database);
 app.use('/login', login)
 app.use('/refrsh', refresh)
 
-app.get('/auth/google',
-  passport.authenticate('google', { scope:
-      [ 'email', 'profile' ] }
-));
-
-app.get( '/auth/google/callback',
-    passport.authenticate( 'google', {
-        successRedirect: '/auth/google/success',
-        failureRedirect: '/auth/google/failure'
-}));
+app.use('/auth/google',google);
+app.use( '/auth/google/callback',callback);
+app.use( '/auth/google/success',success);
 
 // catch 404 and forward to error handler
 app.use(function (req, res, next) {
---
+++ googleauth.js
@@ -0,0 +1,24 @@
+// const jwt = require('json-web-token')
+const jwt = require('jsonwebtoken');
+const passport = require('passport');
+
+exports.google = function(req, res){
+	passport.authenticate('google', { scope: [ 'email', 'profile' ] }
+}
+
+exports.callback = function (req, res){
+    passport.authenticate( 'google', {
+        //successRedirect: '/auth/google/success',
+        //failureRedirect: '/auth/google/failure'
+		jwt.sign({userId: req.user._id}, process.env.ACCESS_TOKEN_SECRET, {expiresIn:'5 min'}, (err, token) => {
+			if(err){
+				res.sendStatus(500);
+			} else {
+				//res.json({token});
+				res.cookie("jwt", token, {secure: false, httpOnly: false})
+				res.redirect(302, '/');
+				//res.send()
+			}
+		});		
+	})
+}
