0c050f3b1877e19d49d9bbde8c2ff398f870be64
saranachon iammongkol <xalanaseon@hotmail.com>
2020-11-21 18:23:34 +0800

    version 3

--- app.js
+++ app.js
@@ -43,12 +43,12 @@ var GoogleStrategy = require( 'passport-google-oauth2' ).Strategy;
 passport.use(new GoogleStrategy({
 	clientID: process.env.GOOGLE_CLIENT_ID,
 	clientSecret: process.env.GOOGLE_CLIENT_SECRET,
-	callbackURL: "https://mi5117701.herokuapp.com/auth/google/callback"
+	callbackURL: 'https://my-awesome-site.com/login/google/return'
 },
 	function(accessToken, refreshToken, profile, done) {
 		
 	});
-));
+);
 // uncomment after placing your favicon in /public
 //app.use(favicon(__dirname + '/public/favicon.ico'));
 app.use(session({
