d82b60339b5a1728e3b1a341b5e3cfc492097ce5
saranachon iammongkol <xalanaseon@hotmail.com>
2020-11-21 22:00:59 +0800

    v4

--- public/script.js
+++ public/script.js
@@ -28,7 +28,7 @@ function submit_create() {
 			document.cookie = 'jwt=; Path=/; Expires=Thu, 01 Jan 1970 00:00:01 GMT;';
 			localStorage.removeItem('jwt')
 			sessionStorage.removeItem('jwt')
-			document.location.href="/";
+			document.location.href="/login";
 		}
 	});
 };
