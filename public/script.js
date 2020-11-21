102eb2264e29a4dbae03da45b945e2b103b0c834
saranachon iammongkol <xalanaseon@hotmail.com>
2020-11-22 03:29:00 +0800

    v4

--- public/script.js
+++ public/script.js
@@ -80,6 +80,7 @@ $(document).ready(function() {
 				document.cookie = 'jwt=; Path=/; Expires=Thu, 01 Jan 1970 00:00:01 GMT;';
 				localStorage.removeItem('jwt')
 				sessionStorage.removeItem('jwt')
+				document.location.href="/login";
 			}
 		});
     });
@@ -119,6 +120,7 @@ $(document).ready(function() {
 				document.cookie = 'jwt=; Path=/; Expires=Thu, 01 Jan 1970 00:00:01 GMT;';
 				localStorage.removeItem('jwt')
 				sessionStorage.removeItem('jwt')
+				document.location.href="/login";
 			}
 		});
     });
